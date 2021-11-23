06447921376a9453df872450f41361ad01a1313c
Client <mejriyasmine23@gmail.com>
2021-11-23 15:32:02 +0100

    adding header component and links to home shop and contact

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
-
+import PerviewCollection from './components/preview-collectioncomponent/preview-collectioncomponent';
+import Header from './components/header/header.component';
 function App() {
   return (
     <div>
+      
       <Router>
+      <Header/>
       <Switch>
         <Route exact  path='/' component={HomePage} />
         <Route   path='/shop' component={ShopPage} />
